f836eb08a4e984853884afd5184db8f2395b0ab9
GabrieladaRocha <gabrieladarocha570@gmail.com>
2020-08-19 22:36:54 -0300

    Atividade realizada na aula 19/08

--- lista1Python/lista_atividade_001.py
+++ lista1Python/lista_atividade_001.py
@@ -1,9 +1,47 @@
+listaAlunos = []
+
+class Aluno:
+    def __init__(self, name, notas):
+        self.name = name
+        self.notas = notas
+
 class Atividade01:
 
-    def checkAdulthood(self, age):
-        if age >= 18:
-            return 'maior'
-        return 'menor'
+    def alunos(self, nomeAluno, notasAluno):
+        self.name = nomeAluno
+        self.notas = notasAluno
+
+    def calcula(self):
+        for x in range(2):
+            alunos = listaAlunos[x]
+            aux = 0
+            total = 0
+
+            for i in range(3):
+                total = aux + alunos.notas[i]
+                aux = total
+
+            media = total / 3
+            if media >= 7:
+                print("\n ", alunos.name," está aprovado")
+            else:
+                print("\n ", alunos.name," foi reprovado")
+
+
+    def input_alunos(self):
+        instance = Atividade01()
+        for x in range(2):
+            notas = []
+            name= input("Digite o nome do aluno: ")
+            for i in range(3):
+                nota = int(input("Digite a nota: "))
+                notas.append(nota)
+
+            aluno = Aluno(name, notas)
+            listaAlunos.append(aluno)
+
+        instance.calcula()
+
 
     def aplication(self):
         idade = int(input('digite a idade: '))
@@ -12,5 +50,6 @@ class Atividade01:
         return 0
 
 
+
 atividade = Atividade01()
-print(atividade.aplication())
+atividade.input_alunos()
